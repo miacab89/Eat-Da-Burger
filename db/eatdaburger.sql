f53b37898f2b2a3caeecb133ce8b2762ab78b4ff
Mia Cabrera <antemia.kavillari@gmail.com>
2020-03-19 17:15:44 -0500

    fixed some errors

--- db/eatdaburger.sql
+++ db/eatdaburger.sql
@@ -1,16 +1,11 @@
-DROP DATABASE IF EXISTS eartdaburger_db;
+CREATE DATABASE eatdaburger_db; 
+DROP DATABASE IF EXISTS eatdaburger_db; 
 
-CREATE DATABASE eatdaburger_db;
-USE eatdaburger_db;
+USE eatdaburger_db; 
 
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name VARCHAR (255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
-);
+CREATE TABLE burgers (
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR (30) NOT NULL, 
+devoured BOOLEAN DEFAULT false
+); 
 
-SELECT * FROM burgers;
-
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password1234'
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 3300;
+var PORT = process.env.PORT || 3306;
 
 var app = express();
 
